4ba022578d63a9c00855e1e9ce486f1b5fb66e76
Rishabh <baidrishabh443@gmail.com>
2022-02-23 21:56:01 +0530

    updated main in package.json

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ app.use(express.json())
 
 dotenv.config()
 
-const scraper = new Scraper("https://proxybay.github.io")
+// const scraper = new Scraper("https://proxybay.github.io")
 
-app.post('/getMagnet',async (req,res)=>{
-    const name = req.body.name
-    const data = await scraper.scrapeData(name)
-    res.send({data})
-})
+// app.post('/getMagnet',async (req,res)=>{
+//     const name = req.body.name
+//     const data = await scraper.scrapeData(name)
+//     res.send({data})
+// })
 app.get('/',(req,res)=>{
     res.send({message:"ok"})
 })
